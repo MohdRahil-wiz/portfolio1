fe9a4dafc9dd0768ddda62a1530adaf6e5b044c7
MohdRahil-wiz <rahilmohd1515@gmail.com>
2025-09-09 15:18:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".hamb-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
